9cd94e0828be4b0b185314188518f9dfebecb61d
Van1ty77 <148027033+Van1ty77@users.noreply.github.com>
2023-10-30 09:02:46 +0500

    Add files via upload

---
+++ 5.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Указываем URL HTML страницы
+url="http://www.constitution.ru/10003000/10003000-7.htm"
+
+# Получаем содержимое страницы и сохраняем во временный файл
+curl -s "$url" > temp.html
+
+# Создаем массив с нужными буквами
+letters=("Ю" "р" "т" "а" "е" "в")
+
+# Используем цикл для поиска слов, начинающихся на каждую букву и записи количества в файл
+for letter in "${letters[@]}"
+do
+    count=$(grep -o "\<$letter\w*\>" temp.html | wc -l)
+    echo -e "$letter:\n$count\n" >> constitution.html
+done
+
+# Удаляем временный файл
+rm temp.html
